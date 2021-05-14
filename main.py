6460caf148e8e93677024733cdc591fa0eb103e5
Joschka Theissen <joschka.theissen@rwth-aachen.de>
2021-05-14 13:34:16 +0200

    Working concept with very little exception handling

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import ccxt
 from pycoingecko import CoinGeckoAPI
 import pandas as pd
 import numpy as np
-from math import sqrt
-from sklearn.preprocessing import MinMaxScaler
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 binance_test_api_key = 'EtfVQ3mFRKz8CEUDHSXB6QN7CI8P70axWy41XUM2QGKApPZH9CWYZVAlwINNLt5K'
 binance_test_secret_key = 'm2etMNsCWLGIXoo7hs3mxn6hZyqw6D8qY7MYj3xqb8TWkCeWtKJ0E8eSUz2r5Jnh'
 
+# symbols of currencies to include in the index
+# using ccxt symbol conventions
 cherry_picked = [
     'btc',
     'eth',
@@ -23,26 +21,29 @@ cherry_picked = [
     'eos',
     'neo',
     'atom',
-    'miota',
+    'iota',
     'algo',
-    'zec',
     'qtum',
     'nano',
-    'ksm',
 
 ]
+
+# translate coingecko symbols to ccxt/binance symbols
+coingecko_symbol_dict = {
+    'miota': 'iota'
+}
 
 cols = ['id', 'symbol', 'current_price', 'market_cap']
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
 def fetch_index_weights():
     cg = CoinGeckoAPI()
+
+    # cg_cherry_picked = [coingecko_symbol_dict.get(symbol) if coingecko_symbol_dict.get(symbol) else symbol
+    #                     for symbol in cherry_picked]
+
     markets = pd.DataFrame.from_records(cg.get_coins_markets(vs_currency='USD'))
+    markets.replace(coingecko_symbol_dict, inplace=True)
     markets = markets.loc[markets['symbol'].isin(cherry_picked)]
     symbols = markets['symbol'].values
     weights = markets['market_cap'].values
@@ -55,25 +56,37 @@ def fetch_index_weights():
 
 def weighted_buy_order(symbols: np.ndarray, weights: np.ndarray, usd_size: float):
     binance = ccxt.binance()
+    # binance.load_markets()
+    # included_symbols = [symbol for symbol in symbols if f'{symbol.upper()}/BUSD' in binance.symbols]
+    # print(included_symbols)
     binance.set_sandbox_mode(True)
     binance.apiKey = binance_test_api_key
     binance.secret = binance_test_secret_key
 
     binance.load_markets()
-    print(binance.symbols)
+    print(f"Number of available binance symbols: {len(binance.symbols)}")
 
 
 
     before = binance.fetch_balance()['free']
     for symbol, weight in zip(symbols, weights):
-        if f'{symbol.upper()}/USDT' not in binance.symbols:
+        ticker = f'{symbol.upper()}/BUSD'
+        if ticker not in binance.symbols:
             continue
-        price = float(binance.fetch_ticker(f'{symbol.upper()}/USDT').get('last'))
+        price = float(binance.fetch_ticker(ticker).get('last'))
         amount = weight * usd_size / price
-        binance.create_market_buy_order(symbol=f'{symbol.upper()}/USDT', amount=amount)
+        try:
+            order = binance.create_market_buy_order(symbol=ticker, amount=amount)
+        except ccxt.InvalidOrder:
+            print(f"Buy order for {amount} {ticker} is invalid!")
+            print("The order amount might be below the minimum!")
+        else:
+            print(f"Bought {order['amount']:5f} {ticker} at {order['price']:.2f} $")
 
     after = binance.fetch_balance()['free']
+    print("Balances before order execution:")
     print(before)
+    print("Balances after order execution:")
     print(after)
 
     return None
@@ -88,9 +101,8 @@ def rebalance_portfolio(symbols: np.ndarray, weights: np.ndarray):
     print("Rebalancing done!")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("Hi, I will just buy and hodl!")
 
     symbols, weights, sqrt_weights = fetch_index_weights()
 
@@ -98,20 +110,4 @@ if __name__ == '__main__':
 
     weighted_buy_order(symbols, sqrt_weights, 100)
 
-    # cg = CoinGeckoAPI()
-    # markets = cg.get_coins_markets(vs_currency='USD')
-    # print(markets)
-    #
-    # markets_df = pd.DataFrame.from_records(markets)
-    # markets_df = markets_df[cols]
-    # cherry_picked_markets = markets_df.loc[markets_df['symbol'].isin(cherry_picked)]
-    #
-    # cherry_picked_markets['weighting'] = cherry_picked_markets['market_cap'].apply(lambda val: val/cherry_picked_markets['market_cap'].sum())
-    # cherry_picked_markets['sqrt_weighting'] = cherry_picked_markets['market_cap'].apply(lambda val: sqrt(val/cherry_picked_markets['market_cap'].sum()))
-    #
-    # normalizer = MinMaxScaler()
-    # values = cherry_picked_markets['sqrt_weighting'].values
-    # cherry_picked_markets['sqrt_weighting'] = values/values.sum()
-
-
-    print("Done")
+    print("Done, now HODL!")
