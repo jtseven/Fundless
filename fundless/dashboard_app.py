8b760d02781bf0f2cb43b193c5e40321aa223d5d
Joschka Theissen <joschka.theissen@rwth-aachen.de>
2021-08-19 13:00:05 +0200

    Some refactoring in dashboard

--- fundless/dashboard_app.py
+++ fundless/dashboard_app.py
@@ -17,6 +17,75 @@ class User(UserMixin):
         self.id = username
 
 
+################################################################################################################
+#                                                  Layouts                                                     #
+################################################################################################################
+
+def create_dashboard(analytics):
+    # Main Dashboard
+    return html.Div(children=[
+        html.H1('FundLess Dashboard', style=dict(textAlign='center')),
+        html.Div([
+            # update allocation chart every 20 seconds
+            dcc.Interval(id='allocation-interval', interval=20 * 1000, n_intervals=0),
+            # update performance chart every 5 minutes
+            dcc.Interval(id='performance-interval', interval=5 * 60 * 1000, n_intervals=0),
+            html.Div([
+                dcc.Graph(
+                    id='allocation_chart',
+                    figure=analytics.allocation_pie(),
+                    config={
+                        'displayModeBar': False
+                    }
+                )], className='four columns'),
+            html.Div([
+                dcc.Graph(
+                    id='performance_chart',
+                    figure=analytics.performance_chart(),
+                    config={
+                        'displayModeBar': False
+                    }
+                )], className='eight columns')
+        ],
+            className='row')
+    ])
+
+
+# Login screen
+def create_login_layout():
+    return html.Div([dcc.Location(id='url_login', refresh=True),
+                     html.H2('''Please log in to continue:''', id='h1'),
+                     dcc.Input(placeholder='Enter your username',
+                               type='text', id='uname-box', n_submit=0),
+                     dcc.Input(placeholder='Enter your password',
+                               type='password', id='pwd-box', n_submit=0),
+                     html.Button(children='Login', n_clicks=0,
+                                 type='submit', id='login-button'),
+                     html.Div(children='', id='output-state'),
+                     html.Br(),
+                     dcc.Link('Home', href='/')], style=dict(textAlign='center'))
+
+
+# Failed Login
+def create_failed_layout():
+    return html.Div([html.Div([html.H2('Log in Failed. Please try again.'),
+                               html.Br(),
+                               html.Div([create_login_layout()]),
+                               dcc.Link('Home', href='/')
+                               ])  # end div
+                     ])  # end div
+
+
+def create_logout_layout():
+    return html.Div([html.Div(html.H2('You have been logged out - Please login')),
+                     html.Br(),
+                     dcc.Link('Home', href='/')
+                     ], style=dict(textAlign='center'))  # end div
+
+
+################################################################################################################
+#                                            Dashboard Class                                                   #
+################################################################################################################
 class Dashboard:
     app: dash.Dash
     analytics: PortfolioAnalytics
@@ -37,6 +106,15 @@ class Dashboard:
         login_manager.init_app(server)
         login_manager.login_view = '/login'
 
+        # Main Layout
+        self.app.layout = html.Div([
+            dcc.Location(id='url', refresh=False),
+            dcc.Location(id='redirect', refresh=True),
+            dcc.Store(id='login-status', storage_type='session'),
+            html.Div(id='user-status-div', style=dict(textAlign='right')),
+            html.Div(id='page-content'),
+        ])
+
         @login_manager.user_loader
         def load_user(username):
             """
@@ -45,33 +123,6 @@ class Dashboard:
                 So we'll simply return a User object with the passed in username.
             """
             return User(username)
-
-        # Login screen
-        login = html.Div([dcc.Location(id='url_login', refresh=True),
-                          html.H2('''Please log in to continue:''', id='h1'),
-                          dcc.Input(placeholder='Enter your username',
-                                    type='text', id='uname-box', n_submit=0),
-                          dcc.Input(placeholder='Enter your password',
-                                    type='password', id='pwd-box', n_submit=0),
-                          html.Button(children='Login', n_clicks=0,
-                                      type='submit', id='login-button'),
-                          html.Div(children='', id='output-state'),
-                          html.Br(),
-                          dcc.Link('Home', href='/')], style=dict(textAlign='center'))
-
-        # Failed Login
-        failed = html.Div([html.Div([html.H2('Log in Failed. Please try again.'),
-                                     html.Br(),
-                                     html.Div([login]),
-                                     dcc.Link('Home', href='/')
-                                     ])  # end div
-                           ])  # end div
-
-        # logout
-        logout = html.Div([html.Div(html.H2('You have been logged out - Please login')),
-                           html.Br(),
-                           dcc.Link('Home', href='/')
-                           ], style=dict(textAlign='center'))  # end div
 
         # Login callback
         @self.app.callback(
@@ -90,96 +141,52 @@ class Dashboard:
             else:
                 return '/login', ''
 
-        # Main Layout
-        self.app.layout = html.Div([
-            dcc.Location(id='url', refresh=False),
-            dcc.Location(id='redirect', refresh=True),
-            dcc.Store(id='login-status', storage_type='session'),
-            html.Div(id='user-status-div', style=dict(textAlign='right')),
-            # html.Br(),
-            # html.Hr(),
-            # html.Br(),
-            html.Div(id='page-content'),
-        ])
-
-        # Main Dashboard
-        dashboard_page = html.Div(children=[
-            html.H1('FundLess Dashboard', style=dict(textAlign='center')),
-            html.Div([
-                # update allocation chart every 20 seconds
-                dcc.Interval(id='allocation-interval', interval=20*1000, n_intervals=0),
-                # update performance chart every 5 minutes
-                dcc.Interval(id='performance-interval', interval=5*60*1000, n_intervals=0),
-                html.Div([
-                    dcc.Graph(
-                        id='allocation_chart',
-                        figure=self.analytics.allocation_pie(),
-                        config={
-                            'displayModeBar': False
-                        }
-                    )], className='four columns'),
-                html.Div([
-                    dcc.Graph(
-                        id='performance_chart',
-                        figure=self.analytics.performance_chart(),
-                        config={
-                            'displayModeBar': False
-                        }
-                    )], className='eight columns')
-            ],
-                className='row')
-        ])
-
+        # Allocation chart update
         @self.app.callback(Output('allocation_chart', 'figure'), Input('allocation-interval', 'n_intervals'))
         def update_allocation_chart(n):
-            print('updating')
             return self.analytics.allocation_pie()
 
+        # Performance chart update
         @self.app.callback(Output('performance_chart', 'figure'), Input('performance-interval', 'n_intervals'))
         def update_performance_chart(n):
             return self.analytics.performance_chart()
 
         # Check login status to show correct login/logout button
-        @self.app.callback(Output('user-status-div', 'children'), Output('login-status', 'data'), [Input('url', 'pathname')])
+        @self.app.callback(Output('user-status-div', 'children'), Output('login-status', 'data'),
+                           [Input('url', 'pathname')])
         def login_status(url):
-            ''' callback to display login/logout link in the header '''
+            """ callback to display login/logout link in the header """
             if hasattr(current_user, 'is_authenticated') and current_user.is_authenticated \
                     and url != '/logout':  # If the URL is /logout, then the user is about to be logged out anyways
                 return dcc.Link('logout', href='/logout'), current_user.get_id()
             else:
                 return dcc.Link('login', href='/login'), 'loggedout'
 
+        # Page forward callback
         @self.app.callback(Output('page-content', 'children'), Output('redirect', 'pathname'),
-                      [Input('url', 'pathname')])
+                           [Input('url', 'pathname')])
         def display_page(pathname):
-            ''' callback to determine layout to return '''
-            # We need to determine two things for everytime the user navigates:
-            # Can they access this page? If so, we just return the view
-            # Otherwise, if they need to be authenticated first, we need to redirect them to the login page
-            # So we have two outputs, the first is which view we'll return
-            # The second one is a redirection to another page is needed
-            # In most cases, we won't need to redirect. Instead of having to return two variables everytime in the if statement
-            # We setup the defaults at the beginning, with redirect to dash.no_update; which simply means, just keep the requested url
+            """ callback to determine layout to return """
             view = None
             url = dash.no_update
             if pathname == '/login':
-                view = login
+                view = create_login_layout()
             elif pathname == '/success':
                 if current_user.is_authenticated:
-                    view = dashboard_page
+                    view = create_dashboard(self.analytics)
                 else:
-                    view = failed
+                    view = create_failed_layout()
             elif pathname == '/logout':
                 if current_user.is_authenticated:
                     logout_user()
-                    view = logout
+                    view = create_logout_layout()
                 else:
-                    view = login
+                    view = create_login_layout()
                     url = '/login'
 
             else:
                 if current_user.is_authenticated:
-                    view = dashboard_page
+                    view = create_dashboard(analytics)
                 else:
                     view = 'Redirecting to login...'
                     url = '/login'
