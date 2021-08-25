cfe3c9664a336ab602cad3fcf7b746ecbfdec0f5
Joschka Theissen <joschka.theissen@rwth-aachen.de>
2021-08-25 18:01:39 +0200

    Removed wrong import

--- fundless/dashboard_app.py
+++ fundless/dashboard_app.py
@@ -2,7 +2,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-import dash_trich_components as dtc
 from dash.dependencies import Output, Input, State
 import secrets
 import flask
