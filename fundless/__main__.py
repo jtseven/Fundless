dc9045f03d1da23d981aa11df24daea8a7b4fd86
Joschka Theissen <jtseven@users.noreply.github.com>
2022-02-13 17:02:54 +0100

    Fix double execution of savings plan

--- fundless/__main__.py
+++ fundless/__main__.py
@@ -1,7 +1,3 @@
-import schedule
-import time
-from datetime import date
-from typing import List
 import coloredlogs
 import logging
 import threading
@@ -9,9 +5,10 @@ import threading
 from trading import TradingBot
 from messages import TelegramBot
 from analytics import PortfolioAnalytics
-from config import Config, IntervalEnum
+from config import Config
 from dashboard_app import Dashboard
 from exchanges import Exchanges
+from savings_plan_scheduler import SavingsPlanScheduler
 """
 
 FundLess is a crypto trading bot that is aiming at a marketcap weighted crypto portfolio - similar to an 'ETF Sparplan'
@@ -27,8 +24,6 @@ trades_csv = 'fundless/data/trades.csv'
 trades_csv_test = 'fundless/data/test_trades.csv'
 order_ids_csv = 'fundless/data/order_ids.csv'
 order_ids_csv_test = 'fundless/data/ids_test.csv'
-
-# logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
 if __name__ == '__main__':
@@ -59,42 +54,18 @@ if __name__ == '__main__':
     else:
         message_bot = None
 
+    scheduler = SavingsPlanScheduler(config, message_bot)
+    savings_plan = threading.Thread(target=scheduler.run, daemon=True)
+    savings_plan.start()
+
     # dashboard as web application
     if config.dashboard_config.dashboard:
         dashboard = Dashboard(config, analytics)
         webapp = threading.Thread(target=dashboard.run_dashboard, daemon=True)
         webapp.start()
+    else:
+        webapp = None
 
-    # automated saving plan execution
-    if message_bot is not None:
-        interval = config.trading_bot_config.savings_plan_interval
-        execution_time = config.trading_bot_config.savings_plan_execution_time
-
-        def job():
-            if isinstance(interval, List):
-                if date.today().day not in interval:
-                    logger.info(f"No savings plan execution today ({date.today().strftime('%d.%m.%y')})")
-                    return
-
-            logger.info(f"Executing savings plan now ({date.today().strftime('%d.%m.%y')})...")
-            if config.trading_bot_config.savings_plan_automatic_execution:
-                message_bot.send('Executing savings plan!')
-                if message_bot.order_planning():
-                    message_bot.execute_order()
-            else:
-                message_bot.ask_savings_plan_execution()
-
-
-        if interval == IntervalEnum.daily:
-            schedule.every().day.at(execution_time).do(job)
-        elif interval == IntervalEnum.weekly:
-            schedule.every().week.at(execution_time).do(job)
-        elif interval == IntervalEnum.biweekly:
-            schedule.every(2).weeks.at(execution_time).do(job)
-        elif isinstance(interval, List):
-            schedule.every().day.at(execution_time).do(job)
-        else:
-            raise ValueError(f'Unknown interval for savings plan execution: {interval}')
-        while True:
-            schedule.run_pending()
-            time.sleep(10)
+    if webapp is not None:
+        webapp.join()
+    savings_plan.join()
